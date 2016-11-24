7863d75cfdb245e2d8b06c7b3eddaa778a55f3bf
Kai Kleefisch <Kai.Kleefisch@gmx.de>
2016-11-24 14:01:33 +0100

    feature-1 Branch hinzugefügt

--- OpenCashRegister/src/application/Main.java
+++ OpenCashRegister/src/application/Main.java
@@ -1,31 +1,33 @@
 package application;
-	
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		final ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 				"classpath:testConfiguration.xml");
 		try {
 			BorderPane root = (BorderPane) applicationContext.getBean("TestBorderPane");
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			root.setCenter(new TextField("Kai ist GEIL"));
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- OpenCashRegister/src/application/TestBorderPane.java
+++ OpenCashRegister/src/application/TestBorderPane.java
@@ -1,10 +1,20 @@
 package application;
 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class TestBorderPane extends BorderPane {
 
 	String name;
+	TextField testTextfeld;
+
+	public TextField getTestTextfeld() {
+		return testTextfeld;
+	}
+
+	public void setTestTextfeld(TextField testTextfeld) {
+		this.testTextfeld = testTextfeld;
+	}
 
 	public String getName() {
 		return name;
@@ -13,6 +23,5 @@ public class TestBorderPane extends BorderPane {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
