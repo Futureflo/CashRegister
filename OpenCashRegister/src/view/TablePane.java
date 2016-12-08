1d921e76d106d5f21bfcb51a170b4086ed43946c
Futureflo <wi14138@lehre.dhbw-stuttgart.de>
2016-12-08 14:51:20 +0100

    CellFactory hinzugefügt

--- OpenCashRegister/src/view/TablePane.java
+++ OpenCashRegister/src/view/TablePane.java
@@ -1,22 +1,55 @@
 package view;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import model.Orderposition;
+import model.Product;
 
 public class TablePane extends TableView<Orderposition> {
 
-	private ObservableList<Orderposition> data;
+	private ObservableList<Orderposition> data = FXCollections.observableArrayList();
 
 	public void init() {
 
 		this.setEditable(true);
 
-		TableColumn productCol = new TableColumn("Produkt");
+		// Product product = new Product();
+		// product.setName("Test");
+		// product.setPrice(2f);
+		//
+		// Orderposition order = new Orderposition();
+		// order.setProduct(product);
+		// order.setCount(2);
+		// data.add(order);
+		TableColumn<Orderposition, Product> productCol = new TableColumn<Orderposition, Product>("Produkt");
 		// productCol.setCellValueFactory(c -> new SimpleStringProperty("" +
 		// orderposition.getProduct().getName()));
+		productCol.setCellValueFactory(new PropertyValueFactory<Orderposition, Product>("name"));
+
+		productCol
+				.setCellFactory(new Callback<TableColumn<Orderposition, Product>, TableCell<Orderposition, Product>>() {
+
+					@Override
+					public TableCell<Orderposition, Product> call(TableColumn<Orderposition, Product> param) {
+
+						TableCell<Orderposition, Product> productCell = new TableCell<Orderposition, Product>() {
+
+							protected void updateItem(Product item, boolean empty) {
+								if (item != null) {
+								}
+							}
+						};
+
+						return productCell;
+					}
+
+				});
+
 		TableColumn countCol = new TableColumn("Anzahl");
 		countCol.setCellValueFactory(new PropertyValueFactory<>("count"));
 		TableColumn priceCol = new TableColumn("Preis");
@@ -25,7 +58,7 @@ public class TablePane extends TableView<Orderposition> {
 		// orderposition.getProduct().getPrice() * orderposition.getCount()));
 
 		this.getColumns().addAll(productCol, countCol, priceCol);
-
+		// this.setItems(data);
 	}
 
 	public ObservableList<Orderposition> getData() {
